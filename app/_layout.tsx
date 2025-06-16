e7d5f508bc57a3ad4d786cf448e70af32b07443d
Jonathan Matos <jonathan.matos2445@gmail.com>
2025-06-16 20:01:09 -0300

    feat: customize style in drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,60 @@
-import { Stack } from 'expo-router';
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function StackLayout() {
-    return (
-        <Stack>
-            <Stack.Screen
-                name='pomodoro'
-                options={{ headerShown: true }}
-            />
-        </Stack >
-    )
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#021123",
+          },
+          headerTintColor: "#FFFFFF",
+          drawerStyle: {
+            backgroundColor: "#021123",
+          },
+          drawerLabelStyle: {
+            color: "#FFFFFF",
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            drawerItemStyle: {
+              display: "none",
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="add-task/index"
+          options={{
+            // headerShown: false,
+            drawerItemStyle: {
+              display: "none",
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="pomodoro"
+          options={{
+            // headerShown: false,
+            drawerLabel: "Timer",
+            title: "",
+          }}
+        />
+
+        <Drawer.Screen
+          name="tasks/index"
+          options={{
+            // headerShown: false,
+            drawerLabel: "Lista de tarefas",
+            title: "",
+          }}
+        />
+      </Drawer>
+      {/* <Stack.Screen name="pomodoro" options={{ headerShown: true }} /> */}
+    </GestureHandlerRootView>
+  );
 }
---
+++ app/tasks/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Tasks() {
+  return (
+    <View>
+      <Text>Pagina para listar Tarefas</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    
+})
